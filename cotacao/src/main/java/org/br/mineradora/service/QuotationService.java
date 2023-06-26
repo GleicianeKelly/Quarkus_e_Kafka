555d6dfa3792ec1ff0b8554b6341da3e8c9a36bd
de Araújo <gleiciane.de.araujo@accenture.com>
2023-06-26 10:32:18 -0300

    Ajustes de metodos no service

--- cotacao/src/main/java/org/br/mineradora/service/QuotationService.java
+++ cotacao/src/main/java/org/br/mineradora/service/QuotationService.java
@@ -4,12 +4,14 @@ import jakarta.inject.Inject;
 import org.br.mineradora.client.CurrencyPriceClient;
 import org.br.mineradora.dto.CurrencyPriceDTO;
 import org.br.mineradora.dto.QuotationDTO;
+import org.br.mineradora.entity.QuotationEntity;
 import org.br.mineradora.message.KafkaEvents;
 import org.br.mineradora.repository.QuotationRepository;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 public class QuotationService {
 
@@ -35,7 +37,36 @@ public class QuotationService {
         }
     }
 
-    private boolean updateCurrenceInfoPrice(CurrencyPriceDTO currencyPriceDTO){
-        
+    private boolean updateCurrenceInfoPrice(CurrencyPriceDTO currencyPriceInfo){
+        BigDecimal currentPrice = new BigDecimal(currencyPriceInfo.USDBRL.getBid());
+        boolean updatePrice = false;
+
+        List<QuotationEntity> quotationList = quotationRepository.findAll().list();
+
+        if (quotationList.isEmpty()){
+            saveQuotation(currencyPriceInfo);
+            updatePrice = true;
+        }else{
+
+            QuotationEntity lastDollarPrice = quotationList
+                    .get(quotationList.size() - 1);
+
+            if(currentPrice.floatValue() > lastDollarPrice.getCurrencyPrice().floatValue()){
+                updatePrice = true;
+                saveQuotation(currencyPriceInfo);
+            }
+        }
+        return updatePrice;
+    }
+
+    private void saveQuotation(CurrencyPriceDTO currencyInfo){
+        QuotationEntity quotation = new QuotationEntity();
+
+        quotation.setDate(new Date());
+        quotation.setCurrencyPrice(new BigDecimal(currencyInfo.USDBRL.getBid()));
+        quotation.setPctChange(currencyInfo.getUSDBRL().getPctChange());
+        quotation.setPair("USD-BRL");
+
+        quotationRepository.persist(quotation);
     }
 }
