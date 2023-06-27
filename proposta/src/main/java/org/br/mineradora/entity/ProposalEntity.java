1326e3e8a2c6979125505b5cfd321af2013696b9
de Araújo <gleiciane.de.araujo@accenture.com>
2023-06-27 12:01:20 -0300

    Inclusao de classes

---
+++ proposta/src/main/java/org/br/mineradora/entity/ProposalEntity.java
@@ -0,0 +1,31 @@
+package org.br.mineradora.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "proposal")
+@Data
+@NoArgsConstructor
+public class ProposalEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String customer;
+
+    @Column(name = "price_tone")
+    private BigDecimal priceTonne;
+
+    private Integer tonnes;
+
+    @Column(name = "proposal_validity_days")
+    private Integer proposalValidityDays;
+
+    private Date created;
+}
