d0fe5e79ce961b740d585f26926f421b82f3393e
de Araújo <gleiciane.de.araujo@accenture.com>
2023-06-27 10:06:14 -0300

    microservice proposta

--- cotacao/src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
+++ cotacao/src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
@@ -5,11 +5,16 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import jakarta.transaction.Transactional;
+import org.br.mineradora.message.KafkaEvents;
 import org.br.mineradora.service.QuotationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class QuotationScheduler {
 
+
+    private final Logger log = LoggerFactory.getLogger(KafkaEvents.class);
 
     @Inject
     QuotationService quotationService;
@@ -17,6 +22,7 @@ public class QuotationScheduler {
     @Transactional
     @Scheduled(every = "35s", identity = "task-job")
     void schedule(){
+        log.info("-- Executando scheduler --");
         quotationService.getCurrencyPriceInfo();
     }
 }
